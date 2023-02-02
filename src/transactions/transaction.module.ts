6e8605c90dbc22c576034cddc858af07d79dfe96
Marcelo Cerqueira <jmarcelo.cjr@gmail.com>
2023-02-02 00:48:15 -0300

    transaction changed

--- src/transactions/transaction.module.ts
+++ src/transactions/transaction.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from 'src/database/database.module';
-import { WalletService } from 'src/wallets/wallet.service';
 import { TransactionController } from './transaction.controller';
 import { TransactionService } from './transaction.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Wallet } from 'src/entities/Wallet.entity';
+import { Transaction } from 'src/entities/Transaction.entity';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [TypeOrmModule.forFeature([Wallet, Transaction])],
   controllers: [TransactionController],
   providers: [TransactionService],
   exports: [TransactionService],
