24a5d744a0cd46ace61d694d0ea0706d9a585e89
Marcelo Cerqueira <jmarcelo.cjr@gmail.com>
2023-02-02 00:47:26 -0300

    migrated to TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TransactionModule } from './transactions/transaction.module';
 import { TransferModule } from './transfers/transfer.module';
 import { WalletModule } from './wallets/wallet.module';
 
 @Module({
-  imports: [WalletModule, TransferModule, TransactionModule],
+  imports: [
+    WalletModule,
+    TransferModule,
+    TransactionModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/entities/*.entity.{js,ts}'],
+      synchronize: process.env.ENV != 'production',
+    }),
+],
   controllers: [],
   providers: [],
 })
